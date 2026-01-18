ddeb1fadbaf4a65e5cda91b37ec38e5fb98218d3
Matthew Simpson <mcsimpson+gh@gmail.com>
2026-01-17 17:03:52 -0800

    extract logic & data & cleanup

--- src/components/SiteFooter/SiteFooter.jsx
+++ src/components/SiteFooter/SiteFooter.jsx
@@ -1,18 +1,23 @@
+// styles
 import "./SiteFooter.scss";
 
 function SiteFooter() {
   return (
     <>
       <div className="sitefooter">
-        <p>
-          Made by{" "}
+        <p className="sitefooter__text">
+          A very silly thing made by{" "}
           <a
             target="_blank"
             rel="noreferrer"
             href="https://matthewcsimpson.dev"
+            className="sitefooter__link"
           >
             Matthew Simpson
           </a>
+        </p>
+        <p className="sitefooter__text sitefooter__text--small">
+          If you can read this you don't need glasses
         </p>
       </div>
     </>
